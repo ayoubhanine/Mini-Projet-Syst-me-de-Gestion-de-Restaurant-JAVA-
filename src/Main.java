58396337421faddc6823a7fdb4857f544a8e64fe
hayat <lahlalihayat70@gmail.com>
2025-11-21 12:31:03 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    Plat P1=new Plat(1,"TAJIN",200.00);
+    PlatSpecial P2=new PlatSpecial(1,"peza",300.00,"plat du jour");
+    System.out.println(P1);
+    System.out.println(P2);
 }
