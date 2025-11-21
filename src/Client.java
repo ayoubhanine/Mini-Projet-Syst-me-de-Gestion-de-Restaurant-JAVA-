9397d6a6c9ee074a1f2c47963a3b3de0868866fa
anas berkaoui <berkaoui.anass12@gmail.com>
2025-11-21 12:20:40 +0100

    remove historique method

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,59 @@
 package PACKAGE_NAME;
 
 public class Client {
+    private int idClent;
+    private String nom;
+    private List<Commande> Commandes;
+
+
+    public Client(int idClent, String nom, List<Commande> commandes) {
+        this.idClent = idClent;
+        this.nom = nom;
+        this.Commandes = new ArrayList<>();
+    }
+
+    public void passerCommande(Commande c){
+        Commandes.add(c);
+    }
+
+
+
+
+
+
+
+
+
+    public List<Commande> getCommandes() {
+        return Commandes;
+    }
+
+    public void setCommandes(List<Commande> commandes) {
+        Commandes = commandes;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getIdClent() {
+        return idClent;
+    }
+
+    public void setIdClent(int idClent) {
+        this.idClent = idClent;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "idClent=" + idClent +
+                ", nom='" + nom + '\'' +
+                ", Commandes=" + Commandes +
+                '}';
+    }
 }
