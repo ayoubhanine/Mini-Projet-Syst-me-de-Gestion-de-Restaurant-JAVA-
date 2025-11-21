7983c3297fbe12e92823a920c70ad270d5a49881
anas berkaoui <berkaoui.anass12@gmail.com>
2025-11-21 12:22:53 +0100

    second commit

--- src/Serveur.java
+++ src/Serveur.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
-
 public class Serveur {
+    private String nom;
+    private int idServeur;
+
+    Serveur(String nom, int idServeur){
+        this.idServeur=idServeur;
+        this.nom=nom;
+    }
+
+    public void prendreCommande(Commande c){
+
+        System.out.println("le serveur "+getNom()+" a pris la commande: "+c.getIdCommande());
+
+    }
+
+
+
+
+
+
+
+
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getIdServeur() {
+        return idServeur;
+    }
+
+    public void setIdServeur(int idServeur) {
+        this.idServeur = idServeur;
+    }
 }
--- src/ServeurVIP.java
+++ src/ServeurVIP.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+public class ServeurVIP extends Serveur{
+    private String TypeSpecial;
 
-public class ServeurVIP {
+
+    ServeurVIP(String nom, int idServeur) {
+        super(nom, idServeur);
+    }
+
+    @Override
+    public void prendreCommande(Commande c){
+
+        System.out.println("le serveurVIP "+getNom()+" a pris la commande: "+c.getIdCommande());
+    }
+
+
+
+
+
+
+
+    public String getTypeSpecial() {
+        return TypeSpecial;
+    }
+
+    public void setTypeSpecial(String typeSpecial) {
+        TypeSpecial = typeSpecial;
+    }
 }
