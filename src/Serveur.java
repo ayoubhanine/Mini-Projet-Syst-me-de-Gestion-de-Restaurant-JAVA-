d17704e506cad9a6afa8cbc862cf83309b038d54
AnasBerkaoui710 <berkaoui.anass12@gmail.com>
2025-11-24 16:53:09 +0100

    Create Serveur.java

---
+++ src/Serveur.java
@@ -0,0 +1,49 @@
+public class Serveur {
+    private String nom;
+    private int idServeur;
+
+    Serveur( int idServeur,String nom){
+        this.idServeur=idServeur;
+        this.nom=nom;
+    }
+
+    public void prendreCommande(Commande c){
+
+        System.out.println("le serveur "+getNom()+" a pris la commande: "+c.getIdCommande());
+
+    }
+
+
+
+
+
+
+
+
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getIdServeur() {
+        return idServeur;
+    }
+
+    public void setIdServeur(int idServeur) {
+        this.idServeur = idServeur;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Serveur{" +
+                "idServeur=" + idServeur +
+                ", nom='" + nom + '\'' +
+                '}';
+    }
+}
