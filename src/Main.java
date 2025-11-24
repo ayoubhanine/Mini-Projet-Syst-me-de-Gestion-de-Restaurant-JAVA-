b78456341fb03331018446222cac88dfea76ad4c
ayoubhanine <ayoubhanine2000@gmail.com>
2025-11-24 16:59:08 +0100

    update main

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,39 @@
 
 
 void main() {
-//Restaurant rest=new Restaurant()
-//    Plat p=new Plat(1,"pizaa",60);
-//    Plat p1=new Plat(2,"tacos",50);
-//    PlatSpecial ps=new PlatSpecial(1,"chwaya",100,"eeeee");
-//
-//    Serveur serv1=new Serveur("ahmed",1);
-//    Serveur serv2=new Serveur("mourad",2);
+    Restaurant resto = new Restaurant("Chez Java");
+    Plat p1 = new Plat(1, "Pizza", 50.0);
+    Plat p2 = new Plat(2, "Spaghetti", 40.0);
+    PlatSpecial p3 = new PlatSpecial(3, "Tacos", 30.0, "Plat du jour");
+    resto.AjouterPlatAumenu(p1);
+    resto.AjouterPlatAumenu(p2);
+    resto.AjouterPlatAumenu(p3);
 
+    Client client = new Client(1, "Omar");
+    Serveur serveur1 = new Serveur(1, "Yassine");
+    ServeurVIP serveurVIP = new ServeurVIP(2, "Karim", "Gold");
 
+    Commande commande = new Commande(1, "21/11/2025",client,serveur1 );
 
+    commande.ajouterPlat(p1);
+    commande.ajouterPlat(p3);
+    client.passerCommande(commande);
 
+    resto.AjouterCommande(commande);
 
+    System.out.println("\n--- MENU DU RESTAURANT ---");
+    resto.afficherMenu();
 
-
-
-
+    System.out.println("\n--- DÉTAILS DE LA COMMANDE ---");
+    System.out.println("Client : " + client.getNom());
+    System.out.println("Serveur : " + serveur1.getNom());
+    System.out.println("Plats commandés : ");
+    for (Plat p : commande.getPlats()) {
+        System.out.println(" - " + p);
+    }
+    System.out.println("TOTAL = " + commande.CalculerTotal() + " DH");
+    System.out.println("\n--- COMMANDES DU RESTAURANT ---");
+    resto.afficherCommandes();
+    System.out.println("\nProgramme terminé avec succès !");
 
 }
